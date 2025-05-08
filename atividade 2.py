713df2d2d212cadcdbeb83690a38559dad8e9622
Miguel Marques <mrqldd@gmail.com>
2025-05-08 19:23:45 -0300

    Adição do cálculo da média total da turma.

--- atividade 2.py
+++ atividade 2.py
@@ -1,9 +1,14 @@
+cont = 0
+soma = 0
 notas = []
 while True:
     nota = float(input("Digite a nota do aluno (ou -1 para sair): "))
     if nota == -1:
         break
     notas.append(nota)
+    soma += nota
+    cont += 1
+media = soma/cont
 
 aprovados = sum(1 for nota in notas if nota >= 5)
 reprovados = sum(1 for nota in notas if nota < 5)
@@ -13,3 +18,5 @@ print("\nResultados:")
 print(f"Quantidade de alunos aprovados: {aprovados}")
 print(f"Quantidade de alunos reprovados: {reprovados}")
 print(f"Quantidade de alunos que fizeram a prova: {total_alunos}")
+print(f"Média total da turma: {media}")
+
